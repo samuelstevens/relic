b3a76aba88b022c6aa46eab75d15b7ae52a1479b
Sam Stevens <samuel.robert.stevens@gmail.com>
2022-09-20 23:32:30 -0400

    Add --conflicts keep option to relic merge

--- relic/cli/merge.py
+++ relic/cli/merge.py
@@ -1,10 +1,17 @@
 import argparse
 import os
 import pathlib
-from typing import Callable, Dict
+import sys
+import typing
+from typing import Callable, Dict, Optional
 
 from .. import experiments, json, projects
 from .lib import logging
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
 
 """
 Trial conflict resolution.
@@ -12,6 +19,7 @@ Trial conflict resolution.
 Sometimes we will have two trials that are in conflict. Which trial should we pick?
 """
 
+ConflictStrategy = Literal["longer", "src", "dst", "none", "both", "keep"]
 ConflictFn = Callable[[experiments.Trial, experiments.Trial], bool]
 
 
@@ -47,11 +55,12 @@ def use_destination_trial(
     return False
 
 
-conflict_choices: Dict[str, ConflictFn] = {
+conflict_choices: Dict[ConflictStrategy, ConflictFn] = {
     "longer": use_longer_trial,
     "src": use_source_trial,
     "dst": use_destination_trial,
     "none": no_resolution,
+    "keep": no_resolution,
 }
 
 
@@ -72,7 +81,7 @@ def add_parser(
     parser_merge.add_argument(
         "--conflicts",
         help="How to resolve trial conflicts. 'longer' chooses the longer trial",
-        choices=conflict_choices.keys(),
+        choices=typing.get_args(ConflictStrategy),
         default="none",
     )
     parser_merge.set_defaults(func=do_merge)
@@ -80,75 +89,119 @@ def add_parser(
 
 def do_merge(args: argparse.Namespace) -> int:
     dst_project = projects.Project(pathlib.Path(args.destination))
+
+    conflict_strategy: ConflictStrategy = args.conflicts
+
     for source in args.sources:
-        merge_source(
+        merge_source_proj(
             projects.Project(pathlib.Path(source)),
             dst_project,
-            conflict_choices[args.conflicts],
+            conflict_strategy,
         )
 
     return 0
 
 
-def merge_source(
-    src_proj: projects.Project, dst_proj: projects.Project, conflict_fn: ConflictFn
+def add_experiment(src_exp: experiments.Experiment, dst_proj: projects.Project) -> None:
+    logging.info(
+        f"Adding experiment '{src_exp}' from source '{src_exp.root}' to destination '{dst_proj}' because experiment is not in destination."
+    )
+
+    dst_exp = experiments.Experiment.new(src_exp.config, dst_proj.root)
+
+    for trial in src_exp:
+        original_model_path = src_exp.model_path(trial["instance"])
+        if not os.path.exists(original_model_path):
+            original_model_path = None  # type: ignore
+        dst_exp.add_trial(trial, model_path=original_model_path)
+
+
+def add_extra_trials(
+    src_exp: experiments.Experiment, dst_exp: experiments.Experiment
+) -> None:
+    logging.debug(
+        "Source experiment %s (%d trials) has more trials than destination experiment (%d trials). Moving all extra (%d) trials to destination.",
+        src_exp,
+        len(src_exp),
+        len(dst_exp),
+        len(src_exp) - len(dst_exp),
+    )
+
+    original_model_path: Optional[pathlib.Path]
+
+    for i, trial in list(enumerate(src_exp))[len(dst_exp) :]:
+        logging.info(f"Adding trial {i} to destination experiment {dst_exp}")
+        original_model_path = src_exp.model_path(trial["instance"])
+        if not os.path.exists(original_model_path):
+            original_model_path = None
+        dst_exp.add_trial(trial, model_path=original_model_path)
+
+
+def merge_source_exp(
+    src_exp: experiments.Experiment,
+    dst_proj: projects.Project,
+    conflict_strategy: ConflictStrategy,
+) -> None:
+    # experiment already exists in dst_proj
+    dst_exp = experiments.Experiment.load(dst_proj.root, src_exp.hash)
+
+    if dst_exp.config != src_exp.config:
+        msg = f"Source experiment {src_exp} has the same hash as destination experiment {dst_exp} but has a different config! Something is very broken!"
+        logging.error(msg)
+        raise RuntimeError(msg)
+
+    logging.debug(
+        "Merging trials from source experiment %s into destination %s.",
+        src_exp,
+        dst_proj,
+    )
+    original_model_path: Optional[pathlib.Path]
+
+    if conflict_strategy == "keep":
+        # Keep all trials
+        for i, src_trial in enumerate(src_exp):
+            original_model_path = src_exp.model_path(i)
+            if not os.path.exists(original_model_path):
+                original_model_path = None
+            del src_trial["instance"]
+            dst_exp.add_trial(src_trial, original_model_path)
+
+    else:
+        assert conflict_strategy in conflict_choices
+        conflict_fn = conflict_choices[conflict_strategy]
+        for i, (src_trial, dst_trial) in enumerate(zip(src_exp, dst_exp)):
+            if src_trial == dst_trial:
+                continue
+
+            try:
+                if conflict_fn(src_trial, dst_trial):
+                    logging.info(
+                        f"Replacing trial {i} in experiment {dst_exp} from experiment {src_exp}."
+                    )
+                    original_model_path = src_exp.model_path(i)
+                    if not os.path.exists(original_model_path):
+                        original_model_path = None
+                    dst_exp.update_trial(src_trial, original_model_path)
+                else:
+                    logging.info(
+                        f"Keeping trial {i} in destination experiment {dst_exp}; ignoring trial {i} in source experiment {src_exp}."
+                    )
+            except ConflictResolutionError:
+                msg = f"Trial {i} is different between source experiment {src_exp} in {src_exp.root} and destination experiment {dst_exp}! This is very bad!"
+                logging.error(msg)
+                raise RuntimeError(msg)
+
+        if len(src_exp) > len(dst_exp):
+            add_extra_trials(src_exp, dst_exp)
+
+
+def merge_source_proj(
+    src_proj: projects.Project,
+    dst_proj: projects.Project,
+    conflict_strategy: ConflictStrategy,
 ) -> None:
     for src_exp in experiments.load_all(src_proj):
         if not experiments.Experiment.exists(dst_proj.root, src_exp.hash):
-            logging.info(
-                f"Adding experiment '{src_exp}' from source '{src_proj}' to destination '{dst_proj}' because experiment is not in destination."
-            )
-
-            dst_exp = experiments.Experiment.new(src_exp.config, dst_proj.root)
-
-            for trial in src_exp:
-                original_model_path = src_exp.model_path(trial["instance"])
-                if not os.path.exists(original_model_path):
-                    original_model_path = None  # type: ignore
-                dst_exp.add_trial(trial, model_path=original_model_path)
-
-            continue
-
-        # experiment exists in destination_dir
-        dst_exp = experiments.Experiment.load(dst_proj.root, src_exp.hash)
-
-        if dst_exp.config != src_exp.config:
-            msg = f"Source experiment {src_exp} has the same hash as destination experiment {dst_exp} but has a different config! Something is very broken!"
-            logging.error(msg)
-            raise RuntimeError(msg)
-
-        logging.debug(
-            f"Merging trials from source experiment {src_exp} into destination {dst_proj}."
-        )
-
-        for i, (src_trial, dst_trial) in enumerate(zip(src_exp, dst_exp)):
-            if src_trial != dst_trial:
-                try:
-                    if conflict_fn(src_trial, dst_trial):
-                        logging.info(
-                            f"Replacing trial {i} in experiment {dst_exp} from experiment {src_exp}."
-                        )
-                        original_model_path = src_exp.model_path(i)
-                        if not os.path.exists(original_model_path):
-                            original_model_path = None  # type: ignore
-                        dst_exp.update_trial(src_trial, original_model_path)
-                    else:
-                        logging.info(
-                            f"Keeping trial {i} in destination experiment {dst_exp}; ignoring trial {i} in source experiment {src_exp}."
-                        )
-                except ConflictResolutionError:
-                    msg = f"Trial {i} is different between source experiment {src_exp} in {src_proj} and destination experiment {dst_exp}! This is very bad!"
-                    logging.error(msg)
-                    raise RuntimeError(msg)
-
-        if len(src_exp) > len(dst_exp):
-            logging.debug(
-                f"Source experiment {src_exp} ({len(src_exp)} trials) has more trials than destination experiment ({len(dst_exp)} trials). Moving all extra ({len(src_exp) - len(dst_exp)}) trials to destination."
-            )
-
-        for i, trial in list(enumerate(src_exp))[len(dst_exp) :]:
-            logging.info(f"Adding trial {i} to destination experiment {dst_exp}")
-            original_model_path = src_exp.model_path(trial["instance"])
-            if not os.path.exists(original_model_path):
-                original_model_path = None  # type: ignore
-            dst_exp.add_trial(trial, model_path=original_model_path)
+            add_experiment(src_exp, dst_proj)
+        else:
+            merge_source_exp(src_exp, dst_proj, conflict_strategy)
--- tests/test_cli_merge.py
+++ tests/test_cli_merge.py
@@ -248,3 +248,42 @@ def test_merge_destination1() -> None:
 
         assert project_a_experiments[0][0]["finished"] is True
         assert "finished" not in project_a_experiments[0][1]
+
+
+def test_merge_keep_all_trials() -> None:
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    cli.merge.add_parser(subparsers)
+
+    with tempfile.TemporaryDirectory() as true_root:
+        root_a = pathlib.Path(true_root) / "project_a"
+        root_a.mkdir()
+        project_a = projects.Project.new(root_a)
+
+        root_b = pathlib.Path(true_root) / "project_b"
+        root_b.mkdir()
+        project_b = projects.Project.new(root_b)
+
+        args = parser.parse_args(
+            ["merge", str(root_b), str(root_a), "--conflicts", "keep"]
+        )
+
+        experiment_a = experiments.Experiment.new({"file": "0.txt"}, project_a.root)
+        experiment_a.add_trial({"finished": True})
+        experiment_a.add_trial({})
+
+        experiment_b = experiments.Experiment.new({"file": "0.txt"}, project_b.root)
+        experiment_b.add_trial({"finished": True})
+        experiment_b.add_trial({"finished": True})
+
+        cli.merge.do_merge(args)
+
+        project_a_experiments = list(experiments.load_all(project_a))
+
+        assert len(project_a_experiments) == 1
+        assert len(project_a_experiments[0]) == 4
+
+        assert project_a_experiments[0][0]["finished"] is True
+        assert "finished" not in project_a_experiments[0][1]
+        assert project_a_experiments[0][2]["finished"] is True
+        assert project_a_experiments[0][3]["finished"] is True
